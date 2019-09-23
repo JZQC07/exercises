b7e583525dae8c7d60e48208271810be772acc3c
Jonatan Schultz <jonatan_schultz@hotmail.com>
2019-09-23 13:58:34 +0200

    klar

---
+++ vektorer20/arraysovntva/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace arrays20e
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] namnlista = new string[5];
+
+            Console.WriteLine("Vilka 5 namn vill du mata in??");
+            for (int i = 0; i < namnlista.Length; i++)
+            {
+                try 
+                {
+                    namnlista[i] = Console.ReadLine();
+                }
+                catch
+                {
+                    Console.WriteLine("Ange dina namn korrekt. Använd bara bokstäver.");
+                }
+                
+            }
+            foreach (var item in namnlista)
+            {
+                Console.WriteLine("Namnen du valde var:");
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
---
+++ vektorer20/metoder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello(); //Startar metoden Hello...
+        }
+        static void Hello()
+        {
+            string sentence = "This is a sentence:";
+
+            Console.WriteLine(sentence);
+        }
+    }
+}
